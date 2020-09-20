d8f9cb66ba11f158aa6fe8b5290ece232a66cb3c
Hoesung Jung <jrps2212@gmail.com>
2020-09-20 14:18:38 +0900

    Initial commit

---
+++ test.py
@@ -0,0 +1,31 @@
+import pyhash 
+
+bit_vector = [0] * 20
+
+#  Non cryptographic hash functions(Murmur and FNV)
+fnv = pyhash.fnv1_32()
+murmur = pyhash.murmur3_32()
+
+# Calculate the output of FNV and Murmur has functions for Pikachu and Charmander
+fnv_pika = fnv("Pikachu") % 20
+fnv_char = fnv("Charmander") % 20
+murmur_pika = murmur("Pikachu") % 20
+murmur_char = murmur("Charmander") % 20
+
+
+print(fnv_pika)
+print(fnv_char)
+print(murmur_pika)
+print(murmur_char)
+
+bit_vector[fnv_pika] = 1 
+bit_vector[murmur_pika] = 1
+bit_vector[fnv_char] = 1
+bit_vector[murmur_char] = 1
+
+#A wild Bulbasaur appears!
+fnv_bulb = fnv("Bulbasaur") % 20
+murmur_bulb = murmur("Bulbasaur") % 20
+
+print(bit_vector[fnv_bulb])
+print(bit_vector[murmur_bulb])
